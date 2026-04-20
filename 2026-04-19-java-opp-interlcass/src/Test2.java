bf4e780458432d9ed408268c3f88bc7734a0d02d
ALearn-Water <535070466@qq.com>
2026-04-20 20:39:43 +0800

    learn interclass and write some code

--- 2026-04-19-java-opp-interlcass/src/Test2.java
+++ 2026-04-19-java-opp-interlcass/src/Test2.java
@@ -1,30 +1,27 @@
 public class Test2 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         //调用
-        goSwimming( new Swim(){
+        goSwimming(new Swim() {
             @Override
             public void swim() {
                 System.out.println("youyong");
             }
         });
-
-        Student s=new Student() {   //因为student里面重写了swim接口，所以可以直接赋值给student变量
+        Student s = new Student() {
             @Override
             public void swim() {
                 System.out.println("youyong");
             }
         };
-        goSwimming(s);
+            goSwimming(s);
 
-        Swim s1=new Swim() {    //还可以直接使用swim接口写内部类，就可以少定义一个stuent类
-            @Override
-            public void swim() {
-                System.out.println("youyong");
-            }
-        };
-        goSwimming(s1);
-    }
-    public static void goSwimming(Swim s){
-        s.swim();
-    }
+            Swim s1 = new Swim() {
+                @Override
+                public void swim() {       //还可以直接使用swim接口写内部类，就可以少定义一个stuent类
+                    System.out.println("youyong");
+                }
+            };
+            goSwimming(s1);
+        }
+        public static void goSwimming(Swim s) { s.swim(); }
 }
