27a5c8b0cec27eeab2092af28c44a7d806973c35
ALearn-Water <535070466@qq.com>
2026-04-30 19:11:58 +0800

    把昨天写的注册代码重写复现,并改进

---
+++ 2024-4-30-java/src/GetApp.java
@@ -0,0 +1,9 @@
+import FightingGame.ui.Log;
+import JavaBean.User;
+
+public class GetApp {
+    public static void main(String[] args){
+        Log l=new Log();
+        l.start();
+    }
+}
---
+++ 2024-4-30-java/src/JavaBean/User.java
@@ -0,0 +1,49 @@
+package JavaBean;
+
+import java.util.Random;
+
+public class User {
+    private String id;  //id随机分配不需要构造,也不需要set get方法
+    private String username;    //所以最后只需写两个变量即可,
+    private String password;
+    private boolean state;  //同理,不需要外部输入状态,也不需要set get方法
+
+    public User(){
+        id=CreateId();
+        state=true;//在空参这里也需要提前把id值赋好
+    }
+    public User( String username,  String password) {
+        id=CreateId();
+        this.password = password;
+        state=true;
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    //写方法随机创建id
+    public static String CreateId(){
+        Random r=new Random();
+        //需要使用stringbuilder来拼接字符串返回
+        StringBuilder sb=new StringBuilder("hyh");
+        for (int i=0;i<7;i++) { //再拼7个数字
+            int random=r.nextInt(10);
+            sb.append(random);
+        }
+        return sb.toString();
+    }
+
+}
